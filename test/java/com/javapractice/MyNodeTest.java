5b210e90bb5d49b5bf5eed4ac21bd7cff32096d3
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-02 23:23:45 +0530

    Tested : Wrote Junit test to check linked list of 56,30 and 70

--- test/java/com/javapractice/MyNodeTest.java
+++ test/java/com/javapractice/MyNodeTest.java
@@ -4,7 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MyNodeTest {
-    public static void main(String[] args) {
-        System.out.println("Welcome");
+    @Test
+    public void given3NumbersWhenLinkedShouldPassedLinkedListTest(){
+        MyNode<Integer> myFirstNode = new MyNode<>(30);
+        MyNode<Integer> mySecondNode = new MyNode<>(56);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result =  myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+        Assertions.assertTrue(result);
     }
 }
