e75696bc499f7967997bc0cfd98c71da104272fc
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-02 23:22:34 +0530

    Added: Created simple linked list of 56,30 and 70

--- main/java/com/javapractice/MyNode.java
+++ main/java/com/javapractice/MyNode.java
@@ -1,7 +1,19 @@
 package com.javapractice;
 
 public class MyNode<K> {
-    public static void main(String[] args) {
-        System.out.println("Welcome");
+    private K key;
+    private MyNode next;
+
+    public MyNode(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
     }
 }
