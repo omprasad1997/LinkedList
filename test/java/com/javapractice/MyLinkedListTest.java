af9440db57db25dbbfa8169d689e553b78e09bd3
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-03 01:05:37 +0530

    Tested : Wrote Junit test to check linked list of adding 30 and 56 to 70

--- test/java/com/javapractice/MyLinkedListTest.java
+++ test/java/com/javapractice/MyLinkedListTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class MyLinkedListTest {
     @Test
-    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop(){
         MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(56);
