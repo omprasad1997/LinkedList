ed8761712ed16baa960b7d47c7f7198f064d4785
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-02 23:19:04 +0530

    Welcome message

---
+++ test/java/com/javapractice/MyNodeTest.java
@@ -0,0 +1,10 @@
+package com.javapractice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyNodeTest {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
