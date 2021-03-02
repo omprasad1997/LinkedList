21b541170bebf161d8bce227de12ce5972e99905
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-03 02:44:51 +0530

    Tested : Wrote Junit test to check whether first element deleted correctly

--- test/java/com/javapractice/MyLinkedListTest.java
+++ test/java/com/javapractice/MyLinkedListTest.java
@@ -55,4 +55,22 @@ public class MyLinkedListTest {
         Assertions.assertTrue(result);
     }
 
+    @Test
+    public void givenFirstElementWhenDeletedShouldPassLinkedListResult(){
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.append(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNodes();
+        myLinkedList.pop();
+        myLinkedList.printMyNodes();
+        boolean result =  MyLinkedList.head.equals(mySecondNode) &&
+                MyLinkedList.head.getNext().equals(myThirdNode) ;
+        Assertions.assertTrue(result);
+    }
+
 }
