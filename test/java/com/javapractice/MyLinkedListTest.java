fc6e99b51f8ad9b346ae415ab1af2a3aeccc1da5
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-03 01:03:31 +0530

    Tested : Wrote Junit test to check linked list of adding 30 and 50 to 70

---
+++ test/java/com/javapractice/MyLinkedListTest.java
@@ -0,0 +1,23 @@
+package com.javapractice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyLinkedListTest {
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result =  MyLinkedList.head.equals(myThirdNode) &&
+                          MyLinkedList.head.getNext().equals(mySecondNode) &&
+                          MyLinkedList.tail.equals(myFirstNode);
+        Assertions.assertTrue(result);
+    }
+}
--- test/java/com/javapractice/MyNodeTest.java
+++ test/java/com/javapractice/MyNodeTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 public class MyNodeTest {
     @Test
     public void given3NumbersWhenLinkedShouldPassedLinkedListTest(){
-        MyNode<Integer> myFirstNode = new MyNode<>(30);
-        MyNode<Integer> mySecondNode = new MyNode<>(56);
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(70);
 
         myFirstNode.setNext(mySecondNode);
